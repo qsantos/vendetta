35d7b26962e5febb68671e6062fb78e4544748a5
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-12-20 20:51:47 +0100

    Handling views
    World view changes as the player's character moves. When the player clicks
    on the screen, the corresponding position in the world can be retrieved.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <SFML/Graphics.h>
 
 #include "world.h"
@@ -7,7 +8,7 @@
 int main(void)
 {
 	sfVideoMode mode = {800, 600, 32};
-	sfRenderWindow* window = sfRenderWindow_create(mode, "SFML window", sfResize | sfClose, NULL);
+	sfRenderWindow* window = sfRenderWindow_create(mode, "Vendetta 0.2", sfResize | sfClose, NULL);
 	if (window == NULL)
 		exit(1);
 
@@ -20,6 +21,9 @@ int main(void)
 
 	character_t* player = &world.characters[0];
 
+	const sfView* default_view = sfRenderWindow_getDefaultView(window);
+	sfView* world_view = sfView_copy(default_view);
+
 	sfClock* clock = sfClock_create();
 	while (sfRenderWindow_isOpen(window))
 	{
@@ -30,12 +34,23 @@ int main(void)
 		while (sfRenderWindow_pollEvent(window, &event))
 		{
 			if (event.type == sfEvtClosed)
+			{
 				sfRenderWindow_close(window);
+			}
 			else if (event.type == sfEvtKeyPressed)
 			{
-				sfKeyCode k = event.key.code;
-				if (k == sfKeyEscape)
+				if (event.key.code == sfKeyEscape)
 					sfRenderWindow_close(window);
+			}
+			else if (event.type == sfEvtMouseButtonReleased)
+			{
+				sfMouseButtonEvent* e = &event.mouseButton;
+				if (e->button != sfMouseLeft)
+					continue;
+
+				sfVector2i pix = {e->x, e->y};
+				sfVector2f pos = sfRenderWindow_mapPixelToCoords(window, pix, world_view);
+				printf("%i %f %f\n", e->button, pos.x, pos.y);
 			}
 		}
 
@@ -45,7 +60,15 @@ int main(void)
 		world_doRound(&world, duration);
 
 		sfRenderWindow_clear(window, sfBlack);
+
+		sfVector2f pos = {player->x, player->y};
+		sfView_setCenter(world_view, pos);
+		sfRenderWindow_setView(window, world_view);
+
 		draw_world(window, &world);
+
+		sfRenderWindow_setView(window, default_view);
+
 		sfRenderWindow_display(window);
 	}
 
