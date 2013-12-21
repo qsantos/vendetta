7454a39c0be9ffd5d301a5caa9ff62b2460238c0
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-12-21 19:07:49 +0100

    Adding buildings

---
+++ universe/building.h
@@ -0,0 +1,10 @@
+#ifndef BUILDING_H
+#define BUILDING_H
+
+typedef struct kindOf_building kindOf_building_t;
+struct kindOf_building
+{
+	const char* name;
+};
+
+#endif
--- universe/universe.h
+++ universe/universe.h
@@ -3,6 +3,7 @@
 
 #include "material.h"
 #include "mine.h"
+#include "building.h"
 
 typedef struct universe universe_t;
 
@@ -13,6 +14,9 @@ struct universe
 
 	int n_mines;
 	kindOf_mine_t mines[8];
+
+	int n_buildings;
+	kindOf_building_t buildings[10];
 };
 
 void universe_init(universe_t* u);
