e165b918df7eff9d14b1df6d64a0d889568476c8
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-12-23 21:41:18 +0100

    Minor: removing dead code

--- universe/mine.h
+++ universe/mine.h
@@ -5,15 +5,12 @@ typedef struct kindOf_mine kindOf_mine_t;
 
 #include <wchar.h>
 
-#include "material.h"
-
 struct kindOf_mine
 {
 	int id;
 	wchar_t* name;
 
 	int material_id;
-	kindOf_material_t* material;
 };
 
 #endif
