607f82b9469b6d16998a9b270359459bbf1ea715
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-12-24 09:18:33 +0100

    Implemented a progress bar drawing function
    Function takes position coordinates (x,y), size (width, height) and
    progress. The position is rectified to take the border into account.

--- draw.c
+++ draw.c
@@ -41,6 +41,37 @@ void draw_mine(graphics_t* g, mine_t* m)
 	draw_object(g, &m->o, sprite);
 }
 
+#define BORDER_SIZE 1
+static void draw_progressBar(graphics_t* g, float x, float y, float w, float h, float p)
+{
+	static sfRectangleShape* frame    = NULL;
+	static sfRectangleShape* progress = NULL;
+	if (frame == NULL)
+	{
+		frame = sfRectangleShape_create();
+		sfColor fill = {0, 0, 0, 0};
+		sfRectangleShape_setFillColor(frame, fill);
+		sfColor outline = {255, 255, 255, 255};
+		sfRectangleShape_setOutlineThickness(frame, BORDER_SIZE);
+		sfRectangleShape_setOutlineColor(frame, outline);
+
+		progress = sfRectangleShape_create();
+		sfColor inner = {0, 255, 0, 255};
+		sfRectangleShape_setFillColor(progress, inner);
+	}
+	sfVector2f pos = {x+BORDER_SIZE,y+BORDER_SIZE};
+	sfRectangleShape_setPosition(frame, pos);
+	sfRectangleShape_setPosition(progress, pos);
+
+	sfVector2f size = {w-2*BORDER_SIZE, h-2*BORDER_SIZE};
+	sfRectangleShape_setSize(frame, size);
+	size.x *= p;
+	sfRectangleShape_setSize(progress, size);
+
+	sfRenderWindow_drawRectangleShape(g->render, progress, NULL);
+	sfRenderWindow_drawRectangleShape(g->render, frame, NULL);
+}
+
 void draw_building(graphics_t* g, building_t* b)
 {
 	int id = b->t->sprite;
@@ -50,6 +81,8 @@ void draw_building(graphics_t* g, building_t* b)
 	sfVector2f pos = {b->o.x - rect.width/2, b->o.y - rect.height};
 	sfSprite_setPosition(sprite, pos);
 	sfRenderWindow_drawSprite(g->render, sprite, NULL);
+
+	draw_progressBar(g, b->o.x - rect.width/2, b->o.y+1, rect.width, 5, 0.37);
 }
 
 void draw_world(graphics_t* g, world_t* w)
