cabf266daceaf00524145a0fe903a6d842e8b5c9
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-12-26 11:50:45 +0100

    Displaying current equipment in subwindow

--- overlay/swequipment.c
+++ overlay/swequipment.c
@@ -14,6 +14,35 @@ void swequipment_draw(swequipment_t* w, game_t* g)
 {
 	if (!subwindow_draw(&w->w, g->g))
 		return;
+
+	sfText* text = NULL;
+	if (text == NULL)
+	{
+		sfColor color = {255, 255, 255, 255};
+
+		text = sfText_create();
+		sfText_setFont         (text, g->g->font);
+		sfText_setCharacterSize(text, 15);
+		sfText_setColor        (text, color);
+	}
+
+	sfVector2f pos = {w->w.x + 20, w->w.y + 50};
+	for (int i = 0; i < g->u->n_slots; i++)
+	{
+		sfText_setPosition(text, pos);
+		sfText_setUnicodeString(text, (sfUint32*) g->u->slots[i].name);
+		sfRenderWindow_drawText(g->g->render, text, NULL);
+
+		int id = g->player->equipment[i];
+		sfUint32* txt = (sfUint32*) (id >= 0 ? g->u->items[id].name : L"-");
+		pos.x += 150;
+		sfText_setPosition(text, pos);
+		sfText_setUnicodeString(text, txt);
+		sfRenderWindow_drawText(g->g->render, text, NULL);
+		pos.x -= 150;
+
+		pos.y += 20;
+	}
 }
 
 char swequipment_catch(swequipment_t* w, game_t* g, float x, float y, int t)
