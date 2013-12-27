1b29e4c5945da5e5bf9e9578f47a123708935e34
Quentin SANTOS <quentin.santos@worldonline.fr>
2013-12-27 13:37:39 +0100

    Item tooltip in inventory subwindow

--- overlay/swinventory.c
+++ overlay/swinventory.c
@@ -37,6 +37,45 @@ size_t swinventory_materialTooltip(wchar_t* buffer, size_t n, universe_t* u, kin
 	return cur;
 }
 
+size_t swinventory_itemTooltip(wchar_t* buffer, size_t n, universe_t* u, kindOf_item_t* it)
+{
+	size_t cur = 0;
+
+	cur += swprintf(buffer+cur, n-cur, L"%ls", it->name);
+
+	for (int i = 0; i < N_SPECIAL_SKILLS; i++)
+	{
+		float b = it->bonus_special[i];
+		if (b == 0)
+			continue;
+
+		wchar_t* name = u->sskills[i].name;
+		cur += swprintf(buffer+cur, n-cur, L"\n%+.0f %ls", b, name);
+	}
+
+	for (int i = 0; i < u->n_materials; i++)
+	{
+		float b = it->bonus_material[i];
+		if (b == 0)
+			continue;
+
+		wchar_t* name = u->materials[i].skill.name;
+		cur += swprintf(buffer+cur, n-cur, L"\n%+f %ls", b, name);
+	}
+
+	for (int i = 0; i < u->n_iskills; i++)
+	{
+		float b = it->bonus_item[i];
+		if (b == 0)
+			continue;
+
+		wchar_t* name = u->iskills[i].name;
+		cur += swprintf(buffer+cur, n-cur, L"\n%+f %ls", b, name);
+	}
+
+	return cur;
+}
+
 char swinventory_cursor(swinventory_t* w, game_t* g, float x, float y)
 {
 	if (!w->w.visible)
@@ -73,6 +112,34 @@ char swinventory_cursor(swinventory_t* w, game_t* g, float x, float y)
 			continue;
 
 		swinventory_materialTooltip(buffer, 1024, g->u, &g->u->materials[i]);
+		graphics_drawTooltip(g->g, x, y, buffer);
+
+		return 1;
+	}
+
+	pos.y += 20;
+
+	for (int i = 0; i < g->u->n_items; i++)
+	{
+		wchar_t* name = g->u->items[i].name;
+		int amount = g->player->inventory.items[i];
+
+		if (amount == 0)
+			continue;
+
+		pos.y += 20;
+
+		wchar_t buffer[1024];
+		swprintf(buffer, 1024, L"%ls: %i", name, amount);
+
+		sfText_setPosition(text, pos);
+		sfText_setUnicodeString(text, (sfUint32*) buffer);
+
+		sfFloatRect rect = sfText_getGlobalBounds(text);
+		if (!sfFloatRect_contains(&rect, x, y))
+			continue;
+
+		swinventory_itemTooltip(buffer, 1024, g->u, &g->u->items[i]);
 		graphics_drawTooltip(g->g, x, y, buffer);
 
 		return 1;
