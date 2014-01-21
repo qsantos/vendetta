f4a2e316f43a32f33c72b9f5033841fb5cac4e71
Quentin SANTOS <quentin.santos@worldonline.fr>
2014-01-21 21:49:01 +0100

    Added Win64 to the automatic releases

--- release.sh
+++ release.sh
@@ -12,7 +12,8 @@ name="vendetta"
 version=$1
 
 time make -j 4 -C src rebuild
-release="$name-$version-linux-64bits"
+OS="linux-64bits"
+release="$name-$version-$OS"
 archive=${release}.tgz
 ln -s . $release
 (git ls-files; ls start.sh lib/* vendetta) | sed "s/^/$release\//" | xargs -d $'\n' tar zcf $archive
@@ -21,10 +22,26 @@ scp $archive micronos:servers/www/vendetta/dl/ &
 
 time make -j 4 -C src rebuild CC=i586-mingw32msvc-gcc
 mv vendetta vendetta.exe
-release="$name-$version-windows-32bits"
+OS="windows-32bits"
+release="$name-$version-$OS"
 archive=${release}.zip
 ln -s . $release
+mv win32/*.dll .
 (git ls-files; ls *.dll vendetta.exe) | sed "s/^/$release\//" | xargs -d $'\n' zip -qr $archive
+mv *.dll win32/
 rm $release
 mv vendetta.exe vendetta
-scp $archive micronos:servers/www/vendetta/dl/
+scp $archive micronos:servers/www/vendetta/dl/ &
+
+time make -j 4 -C src rebuild CC=x86_64-w64-mingw32-gcc
+mv vendetta vendetta.exe
+OS="windows-64bits"
+release="$name-$version-$OS"
+archive=${release}.zip
+ln -s . $release
+mv win64/*.dll .
+(git ls-files; ls *.dll vendetta.exe) | sed "s/^/$release\//" | xargs -d $'\n' zip -qr $archive
+mv *.dll win64/
+rm $release
+mv vendetta.exe vendetta
+scp $archive micronos:servers/www/vendetta/dl/ &
