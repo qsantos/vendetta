3fc9876d7441ad2551e4c65f6a5506af257ae7cc
Quentin SANTOS <quentin.santos@worldonline.fr>
2014-08-27 10:41:40 +0200

    More compact pool using one indirection level

--- src/world/pool.c
+++ src/world/pool.c
@@ -28,10 +28,14 @@ void pool_init(pool_t* p)
 	p->n_objects = 0;
 	p->a_objects = 0;
 	p->objects = NULL;
+
+	p->n_indices = 0;
+	p->indices = NULL;
 }
 
 void pool_exit(pool_t* p)
 {
+	free(p->indices);
 	for (size_t i = 0; i < p->n_objects; i++)
 		free(p->objects[i]);
 	free(p->objects);
@@ -39,45 +43,85 @@ void pool_exit(pool_t* p)
 
 object_t* pool_new(pool_t* p, uuid_t uuid, size_t size)
 {
-	if (uuid < 0)
-		uuid = p->n_objects + 1;
-
-	if (uuid >= (ssize_t) p->a_objects)
-	{
-		while (uuid >= (ssize_t) p->a_objects)
-			p->a_objects = p->a_objects == 0 ? 1 : 2*p->a_objects;
-		p->objects = CREALLOC(p->objects, object_t*, p->a_objects);
-		memset(p->objects + p->n_objects, 0, (p->a_objects - p->n_objects)*sizeof(object_t*));
-	}
-
-	if (uuid >= (ssize_t) p->n_objects)
-		p->n_objects = uuid+1;
-
-	if (p->objects[uuid] != NULL)
+	if (pool_get(p, uuid) != NULL)
 	{
 		fprintf(stderr, "Trying to re-initate object %li\n", uuid);
 		exit(1);
 	}
 
 	object_t* o = (object_t*) CALLOC(char, size);
-	o->uuid = uuid;
-	p->objects[uuid] = o;
+	o->uuid = uuid >= 0 ? uuid : (uuid_t) p->n_indices;
+	pool_push(p, o);
 	return o;
 }
 
 object_t* pool_get(pool_t* p, uuid_t uuid)
 {
-	if (!(0 <= uuid && uuid < (ssize_t) p->n_objects))
+	if (!(0 <= uuid && uuid < (ssize_t) p->n_indices))
 		return NULL;
-	return p->objects[uuid];
+
+	ssize_t idx = p->indices[uuid];
+	if (idx < 0)
+		return NULL;
+
+	object_t* o = p->objects[idx];
+	if (o->uuid != uuid)
+		return NULL;
+
+	return o;
 }
 
 void pool_del(pool_t* p, object_t* a)
 {
 	if (a == NULL)
 		return;
-	p->objects[a->uuid] = NULL;
+
+	size_t idx = p->indices[a->uuid];
+	pool_swap(p, idx, p->n_objects-1);
+	pool_pop(p);
+
 	free(a);
+}
+
+void pool_push(pool_t* p, object_t* o)
+{
+	if (p->n_objects == p->a_objects)
+	{
+		p->a_objects = p->a_objects == 0 ? 1 : 2*p->a_objects;
+		p->objects = CREALLOC(p->objects, object_t*, p->a_objects);
+	}
+	ssize_t idx = p->n_objects++;
+	p->objects[idx] = o;
+
+	// update indices
+	if ((size_t) o->uuid >= p->n_indices)
+	{
+		size_t a_indices = o->uuid+1;
+		p->indices = CREALLOC(p->indices, ssize_t, a_indices);
+		for (size_t i = p->n_indices; i < a_indices; i++)
+			p->indices[i] = -1;
+		p->n_indices = a_indices;
+	}
+	p->indices[o->uuid] = idx;
+}
+
+void pool_pop(pool_t* p)
+{
+	p->n_objects--;
+
+	// update indices
+	p->indices[p->objects[p->n_objects]->uuid] = -1;
+}
+
+void pool_swap(pool_t* p, size_t a, size_t b)
+{
+	object_t* t = p->objects[a];
+	p->objects[a] = p->objects[b];
+	p->objects[b] = t;
+
+	// update indices
+	p->indices[p->objects[a]->uuid] = a;
+	p->indices[p->objects[b]->uuid] = b;
 }
 
 #define SPECIALIZED(T,K) \
